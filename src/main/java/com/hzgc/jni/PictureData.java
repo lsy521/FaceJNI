1a8a1c3e751d84550d95bbbfa6297567dc0f8fe9
zhaozhe <dgxdks@163.com>
2018-05-18 17:12:05 +0800

    add picturedata

---
+++ src/main/java/com/hzgc/jni/PictureData.java
@@ -0,0 +1,36 @@
+package com.hzgc.jni;
+
+public class PictureData {
+    //图片ID
+    private String imageID;
+
+    //图片二进制数据
+    private byte[] binImage;
+
+    //人脸特征对象,包括特征值和人脸属性
+    private FaceAttribute faceAttr;
+
+    public String getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(String imageID) {
+        this.imageID = imageID;
+    }
+
+    public byte[] getBinImage() {
+        return binImage;
+    }
+
+    public void setBinImage(byte[] binImage) {
+        this.binImage = binImage;
+    }
+
+    public FaceAttribute getFaceAttr() {
+        return faceAttr;
+    }
+
+    public void setFaceAttr(FaceAttribute faceAttr) {
+        this.faceAttr = faceAttr;
+    }
+}
